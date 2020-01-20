d1e0cd3449cfd44c408c998f658449e06b1054bb
Theodoros Vragkos <theodoros.vragkos34@outlook.com>
2020-01-20 11:48:23 +0100

    Ajax call for signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +20,8 @@ import { LoginModalComponent } from './login-modal/login-modal.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/signup-modal/signup-modal.component.ts
+++ src/app/signup-modal/signup-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+declare var $: any;
 
 @Component({
   selector: 'app-signup-modal',
@@ -7,10 +10,39 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./signup-modal.component.scss']
 })
 export class SignupModalComponent implements OnInit {
+  public signupForm: FormGroup;
+  public submitted: boolean;
 
-  constructor(public bsModalRef: BsModalRef) { }
+  constructor(
+    public bsModalRef: BsModalRef,
+    private readonly formBuilder: FormBuilder,
+    ) { }
 
   ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      firstName: ['', Validators.required]
+    });
+  }
+
+  public onSubmit() {
+    this.submitted = true;
+    console.log('form', this.signupForm.value);
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    $.ajax({
+      method: 'POST',
+      url: 'https://www.guitarzoom.com',
+      data: this.signupForm.value,
+      done: (res) => {
+        console.log('POST Succeed', res);
+      },
+      error: (err) => {
+        console.log('POST Error', err);
+      }
+    });
   }
 
 }
